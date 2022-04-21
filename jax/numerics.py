d4affaec734971398a440b25482c87f5c69c7f0b
Googler <noreply@google.com>
2022-04-21 00:19:21 -0700

    (3p) LSC: Remove `Lint as` gpylint comment directives.
    This removes the `# [Lint as:] python{2,3}` header comments that used to tell
    gpylint how to parse the file. Gpylint no longer supports that, so these
    comments are noops that can be removed.
    
    More information: go/remove-lint-as-lsc and b/181807094
    
    BEGIN_PUBLIC
    Remove unused comments related to Python 2 compatibility.
    END_PUBLIC
    
    Tested:
        TAP found no affected targets. No targets were built or tested.
        http://test/OCL:443286009:BASE:443253028:1650520674835:8d4c77fa
    PiperOrigin-RevId: 443297372

--- jax/numerics.py
+++ jax/numerics.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Numerics for bilateral_slice."""
 
 import jax.numpy as jnp
